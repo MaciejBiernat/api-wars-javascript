e095d73fc30f9edc92e64f2946d9d5ba68873fb5
Maciej Biernat <zlysuzin@gmail.com>
2020-03-13 11:03:24 +0100

    register form saves data to dict and check password to repeat password

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 
 app = Flask(__name__)
@@ -10,8 +10,27 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 def route():
     return render_template("index.html" )
 
-@app.route('/registration')
-def register():
+@app.route('/registration', methods=['POST', 'GET'])
+def registration():
+    registration_data = {}
+    if request.method == 'POST':
+        if request.form['psw'] != request.form['psw-repeat']:
+            flash ('Repeated password must be the same, please try again')
+            return render_template("registration.html")
+        registration_data['user'] = request.form['user']
+        registration_data['email'] = request.form['email']
+        registration_data['password'] = request.form['psw']
+        registration_data['psw-repeat'] = request.form['psw-repeat']
+
+
+
+        print(registration_data)
+
+        return render_template("registration.html")
+
+
+
+
     return render_template("registration.html" )
 
 @app.route('/login')
