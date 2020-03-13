b945fed3fe275d092fbfa058cdffdee6b332fd43
Maciej Biernat <zlysuzin@gmail.com>
2020-03-13 17:40:02 +0100

    do table header in index.html

---
+++ static/js/planets.js
@@ -0,0 +1,8 @@
+
+
+
+fetch('https://swapi.co/api/planets/')  // set the path; the method is GET by default, but can be modified with a second parameter
+.then((response) => response.json())  // parse JSON format into JS object
+.then((data) => {
+    console.log(data.name);
+})
