a27b9a1b4c43fa73dbd8221e6e3ea897f4363ff2
Maciej Biernat <zlysuzin@gmail.com>
2020-03-13 19:56:27 +0100

    iterate func over planets in planets.js

--- static/js/planets.js
+++ static/js/planets.js
@@ -7,12 +7,24 @@ let planetsData = fetch('https://swapi.co/api/planets/')
     tableDisplay(data.results);
 });
 
-
+const keys = [
+    'name', 'diameter', 'climate', 'terrain', 'surface_water', 'population'
+]
 
 // let tableDisplay = document.getElementById('tbody');
 
 let tableDisplay = function f(api_data) {
-    for (let row of api_data){
-        console.log(row)
+    for (let row of api_data) {
+        for (key of keys) {
+            console.log(row[key])
+        }
     }
 }
+
+// let insertRow = function f(row) {
+//     for (let key of row){
+//         console.log(row[key])
+//     }
+// }
+
+planetsData;
